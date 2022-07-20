efb8359b1bb9bd197d83a9326c1fbde128fedd04
ppj <1476848452@qq.com>
2022-07-20 21:33:05 +0800

    自动构建测试  1

--- src/main/java/com/example/webdemo/controller/TestController.java
+++ src/main/java/com/example/webdemo/controller/TestController.java
@@ -15,7 +15,7 @@ public class TestController {
     @RequestMapping("/hello")
     @ResponseBody
     public String test(){
-        return "hello"+"test"+"test3"+"自动构建"+"888"+"222";
+        return "ppj";
     }
 
 
