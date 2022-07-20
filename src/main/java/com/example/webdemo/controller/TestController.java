d7e3f5b961539792f018a4d49120b205383c86e2
ppj <1476848452@qq.com>
2022-07-20 14:56:43 +0800

    第一次提交  springboot commit

---
+++ src/main/java/com/example/webdemo/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.example.webdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ppj
+ * @date 2022/07/20
+ */
+@Controller
+public class TestController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String test(){
+        return "hello";
+    }
+
+
+}
